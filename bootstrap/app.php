e9d721d0138f9acea56c78e39c66816bfb6ac158
Chris Bouma <chr1sje.bouma@gmail.com>
2024-06-25 15:59:07 +0200

    Roles toegevoegd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,21 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\IsMedewerker;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands:__DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            // Voeg hier je andere middleware toe
+            'Medewerker' => IsMedewerker::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,22 +20,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create 5 categories
-        \App\Models\Categorie::factory(5)->create();
+        // // Create 5 categories
+        // \App\Models\Categorie::factory(5)->create();
 
-        // Create 10 leveranciers (suppliers)
-        \App\Models\Leverancier::factory(10)->create();
+        // // Create 10 leveranciers (suppliers)
+        // \App\Models\Leverancier::factory(10)->create();
 
-        // Create 10 klanten (customers)
-        \App\Models\Klant::factory(10)->create();
+        // // Create 10 klanten (customers)
+        // \App\Models\Klant::factory(10)->create();
 
-        // Create 20 products
-        \App\Models\Product::factory(20)->create();
+        // // Create 20 products
+        // \App\Models\Product::factory(20)->create();
 
-        // Create 10 voedselpakketten (food packages)
-        \App\Models\Voedselpakket::factory(10)->create();
+        // // Create 10 voedselpakketten (food packages)
+        // \App\Models\Voedselpakket::factory(10)->create();
 
-        // Create 30 pakket details
-        \App\Models\PakketDetail::factory(30)->create();
+        // // Create 30 pakket details
+        // \App\Models\PakketDetail::factory(30)->create();
+
+        $this->call(RollenTableSeeder::class);
     }
 }
