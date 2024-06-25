56eae9a00cacfcfed137f0cefc8049a018f92839
Woenzelaar <112466769+Woenzelaar@users.noreply.github.com>
2024-06-25 11:48:32 +0200

    feat: Add error handling and validation messages to Leverancier views

--- app/Http/Controllers/LeverancierController.php
+++ app/Http/Controllers/LeverancierController.php
@@ -13,6 +13,7 @@ class LeverancierController extends Controller
 
      public function index()
      {
+        // Haal alle leveranciers op uit de database
          $leveranciers = Leverancier::all(); // Haalt alle leveranciers op uit de database
      
          return view('leverancier.index', compact('leveranciers')); // Stuurt de data naar de view
@@ -20,51 +21,21 @@ class LeverancierController extends Controller
 
     public function show($id)
     {
+        // Haal de leverancier op uit de database
         $leverancier = Leverancier::findOrFail($id);
         return view('leverancier.show', compact('leverancier'));
     }
 
     public function edit($id)
     {
+        // Haal de leverancier op uit de database
         $leverancier = Leverancier::findOrFail($id);
         return view('leverancier.edit', compact('leverancier'));
     }
 
     public function update(Request $request, $id)
     {
-        $leverancier = Leverancier::findOrFail($id);
-    
-        $validatedData = $request->validate([
-            'bedrijfsnaam' => 'required|unique:leveranciers,bedrijfsnaam,' . $leverancier->id . '|max:255',
-            'contactpersoon' => 'required|max:255',
-            'telefoonnummer' => 'required|numeric',
-            // Voeg hier meer validatieregels toe
-        ]);
-    
-        $leverancier->bedrijfsnaam = $validatedData['bedrijfsnaam'];
-        $leverancier->contactpersoon = $validatedData['contactpersoon'];
-        $leverancier->telefoonnummer = $validatedData['telefoonnummer'];
-        // Update de rest van de velden
-    
-        $leverancier->save();
-    
-        return redirect()->route('leveranciers.index')->with('success', 'Leverancier succesvol bijgewerkt.');
-    }
-
-    public function destroy($id)
-    {
-        $leverancier = Leverancier::findOrFail($id);
-        $leverancier->delete();
-        return redirect()->route('leverancier.index');
-    }
-
-    public function create()
-    {
-        return view('leverancier.create');
-    }
-
-    public function store(Request $request)
-    {
+        // Valideer de data die binnenkomt
         $validatedData = $request->validate([
             'bedrijfsnaam' => 'required|unique:leveranciers|max:255',
             'contactpersoon' => 'required|max:255',
@@ -110,6 +81,77 @@ class LeverancierController extends Controller
         $leverancier->land = $validatedData['land'];
         $leverancier->email = $validatedData['email'];
         
+
+        // Vul de rest van de velden in
+    
+        $leverancier->save();
+    
+    
+        return redirect()->route('leveranciers.index')->with('success', 'Leverancier succesvol bijgewerkt.');
+    }
+
+    public function destroy($id)
+    {
+        // Haal de leverancier op uit de database
+        $leverancier = Leverancier::findOrFail($id);
+        $leverancier->delete();
+        return redirect()->route('leverancier.index');
+    }
+
+    public function create()
+    {
+        // Laat het formulier zien om een nieuwe leverancier toe te voegen
+        return view('leverancier.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Valideer de data die binnenkomt
+        $validatedData = $request->validate([
+            'bedrijfsnaam' => 'required|unique:leveranciers|max:255',
+            'contactpersoon' => 'required|max:255',
+            'telefoon' => 'required|numeric',
+            'volgende_levering' => 'required|date|after_or_equal:today',
+            'straatnaam' => 'required',
+            'huisnummer' => 'required|numeric',
+            'postcode' => 'required',
+            'land' => 'required',
+            'email' => 'required|email',
+
+            // Voeg hier meer validatieregels toe
+        ], [
+            'bedrijfsnaam.required' => 'Een bedrijfsnaam is verplicht.',
+            'bedrijfsnaam.unique' => 'Deze bedrijfsnaam is al in gebruik.',
+            'bedrijfsnaam.max' => 'De bedrijfsnaam mag niet meer dan 255 tekens bevatten.',
+            'contactpersoon.required' => 'Een contactpersoon is verplicht.',
+            'contactpersoon.max' => 'De contactpersoon mag niet meer dan 255 tekens bevatten.',
+            'telefoon.required' => 'Een telefoonnummer is ongeldig.',
+            'telefoon.numeric' => 'Het telefoonnummer moet numeriek zijn.',
+            'volgende_levering.required' => 'De datum van de volgende levering is verplicht.',
+            'volgende_levering.date' => 'De datum van de volgende levering moet een geldige datum zijn.',
+            'volgende_levering.after_or_equal' => 'De datum van de volgende levering moet in de toekomst liggen.',
+            'straatnaam.required' => 'Een straatnaam is verplicht.',
+            'huisnummer.required' => 'Een huisnummer is verplicht.',
+            'huisnummer.numeric' => 'Het huisnummer moet numeriek zijn.',
+            'postcode.required' => 'Een postcode is verplicht.',
+            'land.required' => 'Een land is verplicht.',
+            'email.required' => 'Een e-mailadres is verplicht.',
+            'email.email' => 'Het e-mailadres moet een geldig e-mailadres zijn.',
+
+            // Voeg hier meer aangepaste foutberichten toe
+        ]);
+    
+        $leverancier = new Leverancier();
+        $leverancier->bedrijfsnaam = $validatedData['bedrijfsnaam'];
+        $leverancier->contactpersoon = $validatedData['contactpersoon'];
+        $leverancier->telefoon = $validatedData['telefoon'];
+        $leverancier->volgende_levering = $validatedData['volgende_levering'];
+        $leverancier->straatnaam = $validatedData['straatnaam'];
+        $leverancier->huisnummer = $validatedData['huisnummer'];
+        $leverancier->postcode = $validatedData['postcode'];
+        $leverancier->land = $validatedData['land'];
+        $leverancier->email = $validatedData['email'];
+
 
         // Vul de rest van de velden in
     
